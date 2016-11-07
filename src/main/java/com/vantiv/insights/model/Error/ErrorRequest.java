26eff78ab2e50cfdae7992018f4b72c9b2e52dd3
nicole-cranon <nicole.cranon@gmail.com>
2016-11-07 15:40:11 -0700

    new error request/response data models

---
+++ src/main/java/com/vantiv/insights/model/Error/ErrorRequest.java
@@ -0,0 +1,7 @@
+package com.vantiv.insights.model.Error;
+
+/**
+ *
+ */
+public class ErrorRequest {
+}
---
+++ src/main/java/com/vantiv/insights/model/Error/ErrorResponse.java
@@ -0,0 +1,7 @@
+package com.vantiv.insights.model.Error;
+
+/**
+ * Created by U999716 on 11/7/2016.
+ */
+public class ErrorResponse {
+}
