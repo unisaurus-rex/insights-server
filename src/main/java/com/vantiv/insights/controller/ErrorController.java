c94bdb203d0df5b3fe9e274ce8380032668a24b2
nicole-cranon <nicole.cranon@gmail.com>
2016-11-08 10:41:33 -0700

    turned DefaultController into ErrorController, got rid of ErrorController

--- src/main/java/com/vantiv/insights/controller/ErrorController.java
+++ src/main/java/com/vantiv/insights/controller/ErrorController.java
@@ -1,7 +1,35 @@
 package com.vantiv.insights.controller;
 
+import com.vantiv.insights.model.Error.ErrorResponse;
+import com.vantiv.insights.model.Error.UndefinedRouteResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  */
-public class ErrorController {
+@RestController
+public class ErrorController extends BaseController{
+    /**
+     * Handle Undefined Routes
+     *
+     * @return
+     */
+    @RequestMapping(path = "/**")
+    public ResponseEntity<UndefinedRouteResponse> undefinedRequest() {
+        UndefinedRouteResponse undefinedRoute = new UndefinedRouteResponse();
+
+
+        return new ResponseEntity<UndefinedRouteResponse>(undefinedRoute, new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Cudstom handler for /error
+     */
+    public ResponseEntity<ErrorResponse>genericErrorRequest() {
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
