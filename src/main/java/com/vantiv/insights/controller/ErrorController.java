ceff4fcd903229b84679a01cf0423d9bed472fa7
nicole-cranon <nicole.cranon@gmail.com>
2016-11-11 09:11:11 -0700

    Removed mapping for /** as it was masking spring defined endpoints

--- src/main/java/com/vantiv/insights/controller/ErrorController.java
+++ src/main/java/com/vantiv/insights/controller/ErrorController.java
@@ -15,24 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class ErrorController
         extends BaseController
         implements org.springframework.boot.autoconfigure.web.ErrorController {
-    /**
-     * Handle Undefined Routes
-     *
-     * @return
-     */
-    @RequestMapping(path = "/**")
-    public ResponseEntity<UndefinedRouteResponse> undefinedRequest() {
-        UndefinedRouteResponse undefinedRoute = new UndefinedRouteResponse();
-
-
-        return new ResponseEntity<UndefinedRouteResponse>(undefinedRoute, new HttpHeaders(), HttpStatus.NOT_FOUND);
-    }
+//    /**
+//     * Handle Undefined Routes
+//     *
+//     * @return
+//     */
+//    @RequestMapping(path = "/**")
+//    public ResponseEntity<UndefinedRouteResponse> undefinedRequest() {
+//        UndefinedRouteResponse undefinedRoute = new UndefinedRouteResponse();
+//
+//
+//        return new ResponseEntity<UndefinedRouteResponse>(undefinedRoute, new HttpHeaders(), HttpStatus.NOT_FOUND);
+//    }
 
     /**
      * Custom handler for /error
      */
     @RequestMapping(path = "/error")
     public ResponseEntity<ErrorResponse> genericErrorRequest() {
+        System.err.println();
         return new ResponseEntity<ErrorResponse>(new ErrorResponse("Internal server error.", "An internal server error occured. Blah blah."), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
