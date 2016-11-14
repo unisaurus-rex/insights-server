f1b3c73163b9f6a3246bd9519271ee1cd37b6159
nicole-cranon <nicole.cranon@gmail.com>
2016-11-14 15:35:11 -0700

    SearchTotalsResponse tot SearchResponseTotals

--- src/main/java/com/vantiv/insights/lib/Search.java
+++ src/main/java/com/vantiv/insights/lib/Search.java
@@ -1,8 +1,8 @@
 package com.vantiv.insights.lib;
 
 import com.vantiv.insights.model.Search.SearchRequest;
+import com.vantiv.insights.model.Search.SearchResponseTotals;
 import com.vantiv.insights.model.Search.SearchResultTotals;
-import com.vantiv.insights.model.Search.SearchTotalsResponse;
 
 /**
  * Library of helper functions for the search operation
@@ -13,24 +13,24 @@ public class Search {
      * </p>
      * Assigns search results and other search totals response body values
      * @param searchRequest - the parsed incoming search request content
-     * @return searchTotalsResponse
+     * @return searchResponseTotals
      */
-    public static SearchTotalsResponse getTotalsSummary (SearchRequest searchRequest) {
+    public static SearchResponseTotals getTotalsSummary (SearchRequest searchRequest) {
 
 
         // keep response fields consistent with request fields of the same
-        SearchTotalsResponse searchTotalsResponse = new SearchTotalsResponse();
-        searchTotalsResponse.setGroupBy(searchRequest.getGroupBy());
-        searchTotalsResponse.setStartDate(searchRequest.getStartDate().toLocalDateTime().toString());
-        searchTotalsResponse.setEndDate(searchRequest.getEndDate().toLocalDateTime().toString());
-        searchTotalsResponse.setTimeframe(searchRequest.getTimeframe());
-        searchTotalsResponse.setLimit(searchRequest.getLimit());
-        searchTotalsResponse.setOffset(searchRequest.getOffset());
+        SearchResponseTotals searchResponseTotals = new SearchResponseTotals();
+        searchResponseTotals.setGroupBy(searchRequest.getGroupBy());
+        searchResponseTotals.setStartDate(searchRequest.getStartDate().toLocalDateTime().toString());
+        searchResponseTotals.setEndDate(searchRequest.getEndDate().toLocalDateTime().toString());
+        searchResponseTotals.setTimeframe(searchRequest.getTimeframe());
+        searchResponseTotals.setLimit(searchRequest.getLimit());
+        searchResponseTotals.setOffset(searchRequest.getOffset());
 
         //TODO get searchResults needs to call to db to get search results
         //BSDAO bsdao;  //get from Spring Context ??//
         SearchResultTotals[] searchResults;
 
-        return searchTotalsResponse;
+        return searchResponseTotals;
     }
 }
