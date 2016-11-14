d63e0f209556c16c365b7fc3e7ae65794679ca8e
nicole-cranon <nicole.cranon@gmail.com>
2016-11-14 15:03:11 -0700

    GroupBy field enumeration

---
+++ src/main/java/com/vantiv/insights/model/Search/GroupBy.java
@@ -0,0 +1,19 @@
+package com.vantiv.insights.model.Search;
+
+/**
+ * GroupBy
+ * </p>
+ * Used to limit possibilities for group by field input
+ */
+public enum GroupBy {
+    year,
+    month,
+    week,
+    day,
+    hour,
+    quarter,
+    card,
+    cardGroup,
+    merchant,
+    merchantGroup
+}
