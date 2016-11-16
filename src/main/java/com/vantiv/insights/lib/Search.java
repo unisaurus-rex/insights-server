3dac1a0a94eab2986bea7d54c4032d75d6b68fea
nicole-cranon <nicole.cranon@gmail.com>
2016-11-16 11:01:54 -0700

    getTotalsSummary->getTotals, added getVolumeTotal, getSpendTotal, getInterchangeTotal, and getSurchargeTotal

--- src/main/java/com/vantiv/insights/lib/Search.java
+++ src/main/java/com/vantiv/insights/lib/Search.java
@@ -1,36 +1,199 @@
 package com.vantiv.insights.lib;
 
-import com.vantiv.insights.model.Search.SearchRequest;
-import com.vantiv.insights.model.Search.SearchResponseTotals;
-import com.vantiv.insights.model.Search.SearchResultTotals;
+import com.vantiv.insights.model.Search.*;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+import javax.naming.directory.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static com.vantiv.insights.model.Search.GroupBy.month;
 
 /**
  * Library of helper functions for the search operation
  */
-public class Search {
+public final class Search {
     /**
-     * getTotalsSummary
+     * getTotals
      * </p>
      * Assigns search results and other search totals response body values
-     * @param searchRequest - the parsed incoming search request content
-     * @return searchResponseTotals
+     *
+     * @param searchRequest the parsed incoming search request content
+     * @return searchResponse
      */
-    public static SearchResponseTotals getTotalsSummary (SearchRequest searchRequest) {
+    public static SearchResponseTotals getTotals(SearchRequest searchRequest) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
 
         // keep response fields consistent with request fields of the same
-        SearchResponseTotals searchResponseTotals = new SearchResponseTotals();
-        searchResponseTotals.setGroupBy(searchRequest.getGroupBy());
-        searchResponseTotals.setStartDate(searchRequest.getStartDate().toLocalDateTime().toString());
-        searchResponseTotals.setEndDate(searchRequest.getEndDate().toLocalDateTime().toString());
-        searchResponseTotals.setTimeframe(searchRequest.getTimeframe());
-        searchResponseTotals.setLimit(searchRequest.getLimit());
-        searchResponseTotals.setOffset(searchRequest.getOffset());
+        SearchResponseTotals searchResponse = new SearchResponseTotals();
+        searchResponse.setGroupBy(searchRequest.getGroupBy());
+        searchResponse.setStartDate(searchRequest.getStartDate().toLocalDateTime().toString());
+        searchResponse.setEndDate(searchRequest.getEndDate().toLocalDateTime().toString());
+        searchResponse.setTimeframe(searchRequest.getTimeframe());
+        searchResponse.setLimit(searchRequest.getLimit());
+        searchResponse.setOffset(searchRequest.getOffset());
 
         //TODO get searchResults needs to call to db to get search results
         //BSDAO bsdao;  //get from Spring Context ??//
-        SearchResultTotals[] searchResults;
+        Integer dimension = 2;
+        SearchResultTotals[] searchResults = new SearchResultTotals[0]; // empty array
+        /**************   Hardcoded Search Results ***********************/
+        SearchResultTotals searchResult1 = new SearchResultTotals();
+        DateTime today1 = new DateTime(DateTimeZone.UTC);
+        searchResult1.setInterchange(1.00);
+        searchResult1.setSpend(20.00);
+        searchResult1.setSurcharge(5.00);
+        searchResult1.setVolume(10.00);
+        searchResult1.setCard("Visa");
+        searchResult1.setCardGroup("VisaGroup");
+        searchResult1.setDate(today1.toLocalDate().toString());
+        searchResult1.setDay(String.valueOf(today1.getDayOfWeek()));
+        searchResult1.setHour(String.valueOf(today1.getHourOfDay()));
+        searchResult1.setMerchant("Amerchant");
+        searchResult1.setMerchantGroup("AMerchantGroup");
+        searchResult1.setMonth(String.valueOf(today1.getMonthOfYear()));
+        Integer quarter1 = Utils.monthToQuarter(today1.getMonthOfYear());
+        searchResult1.setQuarter(quarter1.toString());
+        searchResult1.setWeek(String.valueOf(today1.getWeekOfWeekyear()));
+        searchResult1.setYear(String.valueOf(today1.getYear()));
 
-        return searchResponseTotals;
+        SearchResultTotals searchResult2 = new SearchResultTotals();
+        DateTime today2 = new DateTime(DateTimeZone.UTC).plusMonths(3);
+        searchResult2.setInterchange(90.00);
+        searchResult2.setSpend(100.07);
+        searchResult2.setSurcharge(578.01);
+        searchResult2.setVolume(178.00);
+        searchResult2.setCard("MasterCard");
+        searchResult2.setCardGroup("MasterCardGroup");
+        searchResult2.setDate(today2.toLocalDate().toString());
+        searchResult2.setDay(String.valueOf(today2.getDayOfWeek()));
+        searchResult2.setHour(String.valueOf(today2.getHourOfDay()));
+        searchResult2.setMerchant("SomeMerchant");
+        searchResult2.setMerchantGroup("SomeMerchantGroup");
+        searchResult2.setMonth(String.valueOf(today2.getMonthOfYear()));
+        Integer quarter2 = Utils.monthToQuarter(today2.getMonthOfYear());
+        searchResult2.setQuarter(quarter2.toString());
+        searchResult2.setWeek(String.valueOf(today2.getWeekOfWeekyear()));
+        searchResult2.setYear(String.valueOf(today2.getYear()));
+
+        searchResults = new SearchResultTotals[dimension];
+        searchResults[0] = searchResult1;
+        searchResults[1] = searchResult2;
+
+        searchResponse.setTotalSearchResults(2);
+        searchResponse.setLimit(2);   // shows all 2 / 2 results
+        searchResponse.setOffset(0);  // offset = 0, so results 0 and 1 are are returned
+        /********************* END **************************/
+
+        //Set the searchResults content once marshalled how we want it
+        searchResponse.setSearchResults(searchResults);
+////////////////////////////////////////////////////////////////////////////////
+// Reflection Play
+// set only the groupBY fields passed in, others were note asked for
+//        Class<?> c = searchResponse.getClass();
+//        Method method = c.getMethod("getGroupBy");
+//        GroupBy[] groupByFields = (GroupBy[]) method.invoke(searchResponse);
+//        for(GroupBy groupByField: groupByFields) {
+//            // TODO Use the similar to get correct setter method for given group by field
+//            String methodName = "set" + (groupByField.toString().substring(0,1)).toUpperCase() + groupByField.toString().substring(1);
+////            System.out.println((String) c.getMethod(methodName).invoke(searchResponse, "OYEAH!!!"));  // fields need to be set with appropriate data values
+//        }
+////////////////////////////////////////////////////////////////////////////////////
+        return searchResponse;
     }
+
+    /**
+     * getVolumeTotal
+     * </p>
+     * Assigns search results and other search volume totals response body values
+     *
+     * @param searchRequest the parsed incoming search request content
+     * @return searchResponse
+     */
+    public static SearchResponseVolumeTotal getVolumeTotal(SearchRequest searchRequest){
+        SearchResponseVolumeTotal searchResponse = new SearchResponseVolumeTotal();
+
+        searchResponse.setGroupBy(searchRequest.getGroupBy());
+        searchResponse.setStartDate(searchRequest.getStartDate().toLocalDateTime().toString());
+        searchResponse.setEndDate(searchRequest.getEndDate().toLocalDateTime().toString());
+        searchResponse.setTimeframe(searchRequest.getTimeframe());
+        searchResponse.setLimit(searchRequest.getLimit());
+        searchResponse.setOffset(searchRequest.getOffset());
+
+        // TODO DO STUFF
+
+        return searchResponse;
+    }
+
+    /**
+     * getSpendTotal
+     * </p>
+     * Assigns search results and other search spend totals response body values
+     *
+     * @param searchRequest the parsed incoming search request content
+     * @return searchResponse
+     */
+    public static SearchResponseSpendTotal getSpendTotal(SearchRequest searchRequest){
+        SearchResponseSpendTotal searchResponse = new SearchResponseSpendTotal();
+
+        searchResponse.setGroupBy(searchRequest.getGroupBy());
+        searchResponse.setStartDate(searchRequest.getStartDate().toLocalDateTime().toString());
+        searchResponse.setEndDate(searchRequest.getEndDate().toLocalDateTime().toString());
+        searchResponse.setTimeframe(searchRequest.getTimeframe());
+        searchResponse.setLimit(searchRequest.getLimit());
+        searchResponse.setOffset(searchRequest.getOffset());
+
+        // TODO DO STUFF
+
+        return searchResponse;
+    }
+
+    /**
+     * getInterchangeTotal
+     * </p>
+     * Assigns search results and other search interchange totals response body values
+     *
+     * @param searchRequest the parsed incoming search request content
+     * @return searchResponse
+     */
+    public static SearchResponseInterchangeTotal getInterchangeTotal(SearchRequest searchRequest){
+        SearchResponseInterchangeTotal searchResponse = new SearchResponseInterchangeTotal();
+
+        searchResponse.setGroupBy(searchRequest.getGroupBy());
+        searchResponse.setStartDate(searchRequest.getStartDate().toLocalDateTime().toString());
+        searchResponse.setEndDate(searchRequest.getEndDate().toLocalDateTime().toString());
+        searchResponse.setTimeframe(searchRequest.getTimeframe());
+        searchResponse.setLimit(searchRequest.getLimit());
+        searchResponse.setOffset(searchRequest.getOffset());
+
+        // TODO DO STUFF
+
+        return searchResponse;
+    }
+
+    /**
+     * getSurchargeTotal
+     * </p>
+     * Assigns search results and other search surcharge totals response body values
+     *
+     * @param searchRequest the parsed incoming search request content
+     * @return searchResponse
+     */
+    public static SearchResponseSurchargeTotal getSurchargeTotal(SearchRequest searchRequest){
+        SearchResponseSurchargeTotal searchResponse = new SearchResponseSurchargeTotal();
+
+        searchResponse.setGroupBy(searchRequest.getGroupBy());
+        searchResponse.setStartDate(searchRequest.getStartDate().toLocalDateTime().toString());
+        searchResponse.setEndDate(searchRequest.getEndDate().toLocalDateTime().toString());
+        searchResponse.setTimeframe(searchRequest.getTimeframe());
+        searchResponse.setLimit(searchRequest.getLimit());
+        searchResponse.setOffset(searchRequest.getOffset());
+
+        // TODO DO STUFF
+
+        return searchResponse;
+    }
+
 }
