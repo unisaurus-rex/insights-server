a69831554b65416d191cff115526d1a190f7751c
nicole-cranon <nicole.cranon@gmail.com>
2016-11-16 11:00:38 -0700

    interchange, surcharge, and spend endpoints

--- src/main/java/com/vantiv/insights/controller/SearchTotalsController.java
+++ src/main/java/com/vantiv/insights/controller/SearchTotalsController.java
@@ -1,15 +1,13 @@
 package com.vantiv.insights.controller;
 
 import com.vantiv.insights.lib.Search;
-import com.vantiv.insights.model.Search.SearchRequest;
-import com.vantiv.insights.model.Search.SearchResponseTotals;
-import com.vantiv.insights.model.Search.SearchVolumeTotalResponse;
+import com.vantiv.insights.model.Search.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * SearchTotalsController
@@ -33,7 +31,9 @@ public class SearchTotalsController extends BaseController {
      */
     private static final String basepath = "/search/totals";
     private static final String volumeTotalPath = basepath + "/volume";
-
+    private static final String spendTotalPath = basepath + "/spend";
+    private static final String interchangeTotalPath = basepath + "/interchange";
+    private static final String surchargeTotalPath = basepath + "/surcharge";
 
     /**
      * totalsSearch
@@ -57,21 +57,25 @@ public class SearchTotalsController extends BaseController {
             path = basepath,
             consumes = "application/json",
             produces = "application/json")
-    public ResponseEntity<SearchResponseTotals> totalsSearch(@RequestBody(required = false) SearchRequest searchRequest,
+    @ResponseBody
+    public ResponseEntity<SearchResponseTotals> totalsSearch(@Valid @RequestBody(required = false) SearchRequest searchRequest,
+                                                             BindingResult result,
                                                              @RequestParam(value = "limit", required = false) Integer limit,
                                                              @RequestParam(value = "offset", required = false) Integer offset) throws Exception {
         SearchResponseTotals searchResponse;
 
-        if (searchRequest != null) {
+        if (result.hasErrors()) {
+
+            System.err.println(result.getAllErrors()); //temporary to see errors coming out
+            //TODO How to get exception in Error controller then move this logic there
+
+            return new ResponseEntity<SearchResponseTotals> (new SearchResponseTotals(), HttpStatus.BAD_REQUEST);
+        } else {
             //Need to set limit and offset from the incoming URL query parameters
             searchRequest.setLimit(limit);
             searchRequest.setOffset(offset);
 
-            searchResponse = Search.getTotalsSummary(searchRequest);
-        } else {
-            //TODO How to get exception in Error controller then move this logic there
-
-            return new ResponseEntity<SearchResponseTotals>(new SearchResponseTotals(), HttpStatus.BAD_REQUEST);
+            searchResponse = Search.getTotals(searchRequest);
         }
 
         // TODO some incorrect route usage error
@@ -91,24 +95,145 @@ public class SearchTotalsController extends BaseController {
      * @param searchRequest incoming search request body
      * @param limit         maximum number of results to be returned to the client
      * @param offset        initial index to begin pulling 'limit' results
-     * @return SearchVolumeTotalResponse Contains the total volume data point
+     * @return SearchResponseVolumeTotal Contains the total volume data point
      */
     @PostMapping(
             path = volumeTotalPath,
             consumes = "application/json",
             produces = "application/json")
-    public SearchVolumeTotalResponse volumeTotalsSearch(@RequestBody(required = false) SearchRequest searchRequest,
+    public SearchResponseVolumeTotal volumeTotalSearch(@Valid @RequestBody(required = false) SearchRequest searchRequest,
+                                                        BindingResult result,
                                                         @RequestParam(value = "limit", required = false) Integer limit,
                                                         @RequestParam(value = "offset", required = false) Integer offset) {
-        SearchVolumeTotalResponse searchVolumeResponse = new SearchVolumeTotalResponse();
+        SearchResponseVolumeTotal searchResponse = new SearchResponseVolumeTotal();
 
-        if (searchRequest != null) {
+        if (result.hasErrors()) {
+            // TODO some incorrect route usage error
+        } else {
             // TODO call some Search library function to handle request manipulation/management
+            //Need to set limit and offset from the incoming URL query parameters
+            searchRequest.setLimit(limit);
+            searchRequest.setOffset(offset);
+
+            searchResponse = Search.getVolumeTotal(searchRequest);
         }
 
-        // TODO some incorrect route usage error
+        return searchResponse;
+    }
 
-        return searchVolumeResponse;
+    /**
+     * spendTotalSearch
+     * <p/>
+     * Handle requests to
+     * /search/totals/spend
+     * /search/totals/spend?limit=limit
+     * /search/totals/spend?offset=offset
+     * /search/totals/spend?limit=limit&offset=offset
+     *
+     * @param searchRequest incoming search request body
+     * @param limit         maximum number of results to be returned to the client
+     * @param offset        initial index to begin pulling 'limit' results
+     * @return SearchResponseSpendTotal Contains the total spend data point
+     */
+    @PostMapping(
+            path = spendTotalPath,
+            consumes = "application/json",
+            produces = "application/json")
+    public SearchResponseSpendTotal spendTotalSearch(@Valid @RequestBody(required = false) SearchRequest searchRequest,
+                                                      BindingResult result,
+                                                      @RequestParam(value = "limit", required = false) Integer limit,
+                                                      @RequestParam(value = "offset", required = false) Integer offset) {
+        SearchResponseSpendTotal searchResponse = new SearchResponseSpendTotal();
+
+        if (result.hasErrors()) {
+            // TODO some incorrect route usage error
+        } else {
+            // TODO call some Search library function to handle request manipulation/management
+            //Need to set limit and offset from the incoming URL query parameters
+            searchRequest.setLimit(limit);
+            searchRequest.setOffset(offset);
+
+            searchResponse = Search.getSpendTotal(searchRequest);
+        }
+
+        return searchResponse;
+    }
+    
+    /**
+     * interchangeTotalSearch
+     * <p/>
+     * Handle requests to
+     * /search/totals/interchange
+     * /search/totals/interchange?limit=limit
+     * /search/totals/interchange?offset=offset
+     * /search/totals/interchange?limit=limit&offset=offset
+     *
+     * @param searchRequest incoming search request body
+     * @param limit         maximum number of results to be returned to the client
+     * @param offset        initial index to begin pulling 'limit' results
+     * @return SearchResponseInterchangeTotal Contains the total interchange data point
+     */
+    @PostMapping(
+            path = interchangeTotalPath,
+            consumes = "application/json",
+            produces = "application/json")
+    public SearchResponseInterchangeTotal interchangeTotalSearch(@Valid @RequestBody(required = false) SearchRequest searchRequest,
+                                                            BindingResult result,
+                                                            @RequestParam(value = "limit", required = false) Integer limit,
+                                                            @RequestParam(value = "offset", required = false) Integer offset) {
+        SearchResponseInterchangeTotal searchResponse = new SearchResponseInterchangeTotal();
+
+        if (result.hasErrors()) {
+            // TODO some incorrect route usage error
+        } else {
+            // TODO call some Search library function to handle request manipulation/management
+            //Need to set limit and offset from the incoming URL query parameters
+            searchRequest.setLimit(limit);
+            searchRequest.setOffset(offset);
+
+            searchResponse = Search.getInterchangeTotal(searchRequest);
+        }
+
+        return searchResponse;
+    }
+
+    
+    /**
+     * surchargeTotalSearch
+     * <p/>
+     * Handle requests to
+     * /search/totals/surcharge
+     * /search/totals/surcharge?limit=limit
+     * /search/totals/surcharge?offset=offset
+     * /search/totals/surcharge?limit=limit&offset=offset
+     *
+     * @param searchRequest incoming search request body
+     * @param limit         maximum number of results to be returned to the client
+     * @param offset        initial index to begin pulling 'limit' results
+     * @return SearchResponseSurchargeTotal Contains the total surcharge data point
+     */
+    @PostMapping(
+            path = surchargeTotalPath,
+            consumes = "application/json",
+            produces = "application/json")
+    public SearchResponseSurchargeTotal surchargeTotalSearch(@Valid @RequestBody(required = false) SearchRequest searchRequest,
+                                                            BindingResult result,
+                                                            @RequestParam(value = "limit", required = false) Integer limit,
+                                                            @RequestParam(value = "offset", required = false) Integer offset) {
+        SearchResponseSurchargeTotal searchResponse = new SearchResponseSurchargeTotal();
+
+        if (result.hasErrors()) {
+            // TODO some incorrect route usage error
+        } else {
+            // TODO call some Search library function to handle request manipulation/management
+            //Need to set limit and offset from the incoming URL query parameters
+            searchRequest.setLimit(limit);
+            searchRequest.setOffset(offset);
+
+            searchResponse = Search.getSurchargeTotal(searchRequest);
+        }
+
+        return searchResponse;
     }
 
 
